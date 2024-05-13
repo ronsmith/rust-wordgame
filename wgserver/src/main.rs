39f4cc51ed609f66856c73d9418d53e9d87f38b7
Ron Smith <ron.smith@thataintworking.com>
2024-05-13 17:10:06 -0400

    Added Actix and some dummy routes

--- wgserver/src/main.rs
+++ wgserver/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn root() -> impl Responder {
+    HttpResponse::Ok().body("<h1>Welcome to WordGame</h1>")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("<h1>Hello WordGame!</h1>")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(root)
+            .service(echo)
+            .route("/hello", web::get().to(hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
